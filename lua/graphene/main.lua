d1289616d7c9f34f39b18be297ab6ee6e0eac0e6
narutoxy <79555780+NarutoXY@users.noreply.github.com>
2021-12-25 14:47:36 +0530

    rm print

--- lua/graphene/main.lua
+++ lua/graphene/main.lua
@@ -5,22 +5,21 @@ if starting == 1 then
 	starting = 0
 end
 
-	local textCode = table.concat(vim.api.nvim_buf_get_lines(0, starting, ending, false), "\n")
+local textCode = table.concat(vim.api.nvim_buf_get_lines(0, starting, ending, false), "\n")
 
-	local postBody = {
-		code = textCode,
-		lang = vim.bo.filetype,
-		theme = options.theme,
-		format = options.format,
-		upscale = options.upscale,
-		font = options.font,
-		lineNumber = options.lineNumber,
-		border = {
-			thickness = options.thickness,
-			radius = options.radius,
-			colour = options.color,
-		},
-	}
-	
-	print(textCode)
-	vim.api.nvim_exec(string.format([[silent !curl -X POST 'https://teknologi-umum-graphene.fly.dev/api' -H 'content-type: application/json' -d '%s' --output %s.%s]], vim.json.encode(postBody), options.outputFile, options.format), true)
+local postBody = {
+	code = textCode,
+	lang = vim.bo.filetype,
+	theme = options.theme,
+	format = options.format,
+	upscale = options.upscale,
+	font = options.font,
+	lineNumber = options.lineNumber,
+	border = {
+		thickness = options.thickness,
+		radius = options.radius,
+		colour = options.color,
+	},
+}
+
+vim.api.nvim_exec(string.format([[silent !curl -X POST 'https://teknologi-umum-graphene.fly.dev/api' -H 'content-type: application/json' -d '%s' --output %s.%s]], vim.json.encode(postBody), options.outputFile, options.format), true)
