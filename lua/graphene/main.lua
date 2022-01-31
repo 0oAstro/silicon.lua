959554e5ae6ba850d382143cd62dae258245cd02
Astro (Naruto Uzumaki) <github@sandy007.anonaddy.com>
2022-01-31 15:29:19 +0530

    give graphene some love

--- lua/graphene/main.lua
+++ lua/graphene/main.lua
@@ -1,9 +1,6 @@
 local options = require("graphene.config")("get")
 
-local starting, ending = vim.api.nvim_buf_get_mark(0, "<")[1], vim.api.nvim_buf_get_mark(0, ">")[2]
-if starting == 1 then
-	starting = 0
-end
+local starting, ending = vim.api.nvim_buf_get_mark(0, "<")[1] - 1, vim.api.nvim_buf_get_mark(0, ">")[1] - 1
 
 local textCode = table.concat(vim.api.nvim_buf_get_lines(0, starting, ending, false), "\n")
 
@@ -22,4 +19,8 @@ local postBody = {
 	},
 }
 
-vim.api.nvim_exec(string.format([[silent !curl -X POST 'https://teknologi-umum-graphene.fly.dev/api' -H 'content-type: application/json' -d '%s' --output %s.%s]], vim.json.encode(postBody), options.outputFile, options.format), true)
+if ending > 0 then
+  vim.api.nvim_exec(string.format([[silent !curl -X POST 'https://teknologi-umum-graphene.fly.dev/api' -H 'content-type: application/json' -d '%s' >> %s.%s]], vim.json.encode(postBody), options.outputFile, options.format), true)
+else
+  vim.notify("Please select code in visual mode first.", vim.log.levels.WARN)
+end
