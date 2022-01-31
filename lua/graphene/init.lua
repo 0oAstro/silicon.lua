12dd1b8ebb5d043c1c568e5e103d80d706d66115
Astro (Naruto Uzumaki) <github@sandy007.anonaddy.com>
2022-01-31 15:34:27 +0530

    Graphene command

--- lua/graphene/init.lua
+++ lua/graphene/init.lua
@@ -1,3 +1,10 @@
-return function (opts)
+local init = {} 
+init.setup = function (opts)
 	require("graphene.config").setup("user", opts)
 end
+
+init.visualise = function ()
+  require("graphene.main")
+end
+
+return init
---
+++ plugin/graphene.lua
@@ -0,0 +1,1 @@
+vim.cmd([[command! -nargs=0 Graphene lua require("graphene").visualise()]])
